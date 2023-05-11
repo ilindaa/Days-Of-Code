c524b867fa1c34c325c248a75ce928d44fda1e21
Linda Mei <85714837+ilindaa@users.noreply.github.com>
2023-05-11 00:18:15 -0400

    day 2

---
+++ Day2/animations.js
@@ -0,0 +1,7 @@
+function changeCat() {
+    document.getElementById("pic").src = "../Day1/Images/cat.jpg";
+}
+
+function changeBack() {
+    document.getElementById("pic").src = "../Day1/Images/baby_pom.jpg";
+}
